3749020e918e1347647b7ccf0dd0566217479add
Amani Al Faqih <amanialfaqih12@gmail.com>
2026-03-30 11:45:50 +0700

    update project modul 5 + login auth

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,49 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
 use App\Models\Book;
 
-Route::get('/', function () {
+/*
+|--------------------------------------------------------------------------
+| LOGIN
+|--------------------------------------------------------------------------
+*/
 
-    $books = Book::latest()->take(8)->get();
+// halaman pertama = login
+Route::get('/', [AuthController::class, 'showLogin'])
+    ->name('login')
+    ->middleware('guest');
 
-    return view('frontend.home', compact('books'));
+// proses login
+Route::post('/login', [AuthController::class, 'login']);
+
+// logout
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+
+/*
+|--------------------------------------------------------------------------
+| SETELAH LOGIN (WAJIB LOGIN)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth')->group(function () {
+
+    // HOME (yang ada banner + books)
+    Route::get('/home', function () {
+
+        $books = Book::latest()->take(8)->get();
+
+        return view('frontend.home', compact('books'));
+
+    })->name('home');
+
+    // CRUD BOOK
+    Route::resource('books', BookController::class);
+
+    // CRUD CATEGORY
+    Route::resource('categories', CategoryController::class);
 
 });
-
-Route::resource('books', BookController::class);
-Route::resource('categories', CategoryController::class);
